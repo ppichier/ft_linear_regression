d464467c2ea49d7188a33a03f197d403e40e890f
Pier'antonio PICHIERRI <ppichier@e1r13p13.42.fr>
2020-02-14 16:30:12 +0100

    [ADD] cost visu

--- train.py
+++ train.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def showVisu(X, Y, t0, t1, normalize=True):
+def showCostVisu(N, C):
+    plt.xlabel("iterations")
+    plt.ylabel("cost")
+    plt.plot(list(range(N)), C, 'b')
+    plt.show()
+
+def showRegressionVisu(X, Y, t0, t1, normalize=True):
     Yp = []
     for x in X:
         Yp.append(t0 + t1 * x)
@@ -23,38 +30,32 @@ def cost(X, Y, t0, t1):
 
 def gradient_descent(X, Y,t0, t1, learning_rate, num_iterations):
     N = len(X)
+    cost = []
 
     for i in range(num_iterations):
         t0_gradient = 0.0 
         t1_gradient = 0.0
+        e = 0.0
         for j in range(N):
             t0_gradient += ((t0 + t1 * X[j]) - Y[j])
             t1_gradient += (((t0 + t1 * X[j]) - Y[j]) * X[j])
+            e += abs((t0 + t1 * X[j]) - Y[j])
         t0 =  t0 - (learning_rate * t0_gradient * 1/float(N))
         t1 =  t1 - (learning_rate * t1_gradient * 1/float(N))
-    return [t0,t1]
+        cost.append(e)
+    return [t0, t1, cost]
 
 def normalize(X, Y, initial_t0):
     #Normalize X and Y on 0,1
-    maxX = float(max(X))
-    minX = float(min(X))
-    maxY = float(max(Y))
-    minY = float(min(Y))
-    Xnormalize = list(map((lambda x: (float(x) - minX)/(maxX - minX)), X))
-    Ynormalize = list(map((lambda y: (float(y) - minY)/(maxY - minY)), Y))
-
+    Xnormalize = list(map((lambda x: (float(x) - float(min(X)))/(float(max(X)) - float(min(X)))), X))
+    Ynormalize = list(map((lambda y: (float(y) - float(min(Y)))/(float(max(Y)) - float(min(Y)))), Y))
     #Normalize initial value of t0
-    t0 = (float(initial_t0) - minX)/(maxX - minX)
-    
+    t0 = (float(initial_t0) - float(min(X)))/(float(max(X) - float(min(X))))
     return [Xnormalize, Ynormalize, t0]
 
 def denormalize(X, Y, t0_normalize, t1_normalize):
-    maxX = float(max(X))
-    minX = float(min(X))
-    maxY = float(max(Y))
-    minY = float(min(Y))
-    t1 = t1_normalize * (maxY - minY) / (maxX - minX)
-    t0 = t0_normalize * (maxY - minY) + minY - t1 * minX
+    t1 = t1_normalize * (float(max(Y)) - float(min(Y))) / (float(max(X)) - float(min(X)))
+    t0 = t0_normalize * (float(max(Y)) - float(min(Y))) +float(min(Y)) - t1 * float(min(X))
     return [t0, t1]
 
 def train():
@@ -77,7 +78,7 @@ def train():
         raise Exception(e)
 
     Xn, Yn, initial_t0 = normalize(X, Y, initial_t0)
-    t0_normalize, t1_normalize = gradient_descent(Xn, Yn, initial_t0, initial_t1, learning_rate, num_iterations)
+    t0_normalize, t1_normalize, C = gradient_descent(Xn, Yn, initial_t0, initial_t1, learning_rate, num_iterations)
     t0, t1 = denormalize(X, Y, t0_normalize, t1_normalize)
 
     try:
@@ -91,8 +92,9 @@ def train():
     print("theta1 = {0:.3f}".format(t1))
     print("Average error: {0:.3f}".format(cost(X, Y, t0, t1)))
 
-    showVisu(Xn, Yn, t0_normalize, t1_normalize)
-    showVisu(X, Y, t0, t1, False)
+    showRegressionVisu(Xn, Yn, t0_normalize, t1_normalize)
+    showRegressionVisu(X, Y, t0, t1, False)
+    showCostVisu(num_iterations, C)
 
 if __name__== '__main__':
     train()
